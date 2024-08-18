dd33cefc67c49c86becafe6ecb6fcbdae4f53475
Daniel Reeder <danielreeder123@gmail.com>
2024-08-17 20:03:43 -0700

    Got selenium going, working on getting recent reviews

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-<<<<<<< HEAD
-from bs4 import BeautifulSoup as bs
-from requests_html import HTMLSession
-import requests as requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-=======
->>>>>>> a24b204a8363634ebfaa1a327c6c4e16b9d58472
-print("Hello world!")
+browser_driver = Service(
+    '/lib/chromium-browser/chromedriver'
+)
+
+options = Options()
+#options.add_argument("--headless=new")
+
+driver = webdriver.Chrome(service=browser_driver, options=options)
+driver.get("https://www.tripadvisor.com/Hotel_Review-g51862-d2282005-Reviews-Inn_at_the_5th_Eugene-Eugene_Oregon.html")
+
+sort_by = driver.find_element(By.CSS_SELECTOR, ".CkBUi.H")
+sort_by.click()
+print(sort_by.find_element(By.CSS_SELECTOR, "div:first-child").get_attribute("class"))
+
+titles = driver.find_elements(By.CSS_SELECTOR, "div[data-test-target*='review-title']>a>span")
+reviews = driver.find_elements(By.CSS_SELECTOR, "span[data-automation*='reviewText']>span")
+
+for title, review in zip(titles, reviews):
+    print(title.text)
+    print(review.text)
+    print('\n\n')
+
